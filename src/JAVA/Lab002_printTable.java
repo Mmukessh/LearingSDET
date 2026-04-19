58ef260f4ebccd7832a3387cba70ab76413136da
Mukesh <mmukesshvvijaydhagge@gmail.com>
2026-04-19 21:03:43 +0530

    ok

---
+++ src/JAVA/Lab002_printTable.java
@@ -0,0 +1,12 @@
+package JAVA;
+
+public class Lab002_printTable {
+    public static void main(String[] args) {
+        int num=9;
+
+        for (int i = 1; i <=10 ; i++) {
+
+            System.out.println(num*i);
+        }
+    }
+}

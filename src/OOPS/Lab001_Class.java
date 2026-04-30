dd4d29aa048ed7103a3ab070c796a63b2696a439
Mukesh <mmukesshvvijaydhagge@gmail.com>
2026-04-30 20:45:55 +0530

    30 apr 2026

---
+++ src/OOPS/Lab001_Class.java
@@ -0,0 +1,34 @@
+package OOPS;
+
+public class Lab001_Class {
+    public static void main(String[] args) {
+
+        person p =new person();
+        p.name="Mukesh";
+        p.address="Mumbai";
+        p.mobile=987654321;
+
+        System.out.println(p.name);
+        System.out.println(p.address);
+        System.out.println(p.mobile);
+
+        p.speak();
+
+    }
+}
+
+class person {
+
+    String name;
+    String address;
+    long mobile;
+
+    void speak()
+    {
+        System.out.println("speaking !!!!");
+
+    }
+
+
+}
+
